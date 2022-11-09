6bb086095b925c2a0c12fa4bc036040cb7fdd5ed
ozrn <ozerenkevser92@gmail.com>
2022-11-09 23:05:22 +0300

    Add main component, nest it inside app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./Nav";
+import Main from "./Main";
 
 export default function App(){
 
   return(
     <div>
       <Nav />
+      <Main />
     </div>
   )
 }
---
+++ src/Main.js
@@ -0,0 +1,16 @@
+export default function Main() {
+
+  return (
+
+    <section className = "section-main">
+
+      <img src = {require("./assets/photo-grid.png")} className = "main-photo" alt = "random-images" />
+
+      <h1 className = "main-header">Online Experiences</h1>
+
+      <p className = "main-text">Join unique interactive activities led by
+      one-of-a-kind hosts—all without leaving home.</p>
+
+    </section>
+  )
+}
